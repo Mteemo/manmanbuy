c4c3d96fb53f3063131d376437e3290a44cd63a3
15736708762 <2415979366@qq.com>
2019-02-19 20:23:07 +0800

    十大品牌

--- js/brandtitle.js
+++ js/brandtitle.js
@@ -16,4 +16,8 @@ $(function(){
         var brandtitleid =$(this).data('brand-title-id')
         location='brand.html?brandtitleid='+brandtitleid+'&brandtitle='+brandtitle
     })
+
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
 })
